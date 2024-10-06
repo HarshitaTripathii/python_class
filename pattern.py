f96a583214a8fb6c559cbd7c5abc2e936744fedc
HarshitaTripathii <harshitat216@gmail.com>
2024-10-06 09:03:47 +0530

    pattern K triangle

--- pattern.py
+++ pattern.py
@@ -15,17 +15,50 @@
 #         else:
 #             print(" ",end='')
 #    print()
-n=6
-for i in range(1,7):
-    for j in range(i,7):
-        if(i==1):
-            print("*",end='')
-        else:
-            if (j>i and j<6):
-                print(" ",end='')
-            else:
-                print("*", end='')
-    print()
+# n=6
+# for i in range(1,7):
+#     for j in range(i,7):
+#         if(i==1):
+#             print("*",end='')
+#         else:
+#             if (j>i and j<6):
+#                 print(" ",end='')
+#             else:
+#                 print("*", end='')
+#     print()
+
+# n=6
+# for i in range(1,6):
+#     for j in range(1,(i+1)):
+#         if(i==5):
+#             print("*",end='')
+#         else:
+#             if (j>1 and j<i):
+#                 print(" ",end='')
+#             else:
+#                 print("*", end='')
+#     print()
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if(i==n or (i+j)==(n+1)):
+#             print("*",end='')
+#         elif(j!=1 and (i+j)!=2*i):
+#             print(" ",end='')
+#         else:
+#             print("*", end='')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if(i+j<=2*i):
+#             print("*", end='')
+#         elif(i+j==6):
+#             print("*", end='')
+#         else:
+#             print(" ",end='')
+#     print()
 
 
         
