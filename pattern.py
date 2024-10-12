656bf45e2d37730ed9ed2d25245a06f9271bf2cd
HarshitaTripathii <harshitat216@gmail.com>
2024-10-12 11:27:08 +0530

    pattern

--- pattern.py
+++ pattern.py
@@ -53,16 +53,27 @@
 #         else:
 #             print("*", end='')
 #     print()
-n=int(input("enter number of rows"))
-for i in range(1,(n+1)):
-    for j in range(1,(n+1)):
-        if(i+j<=2*i):
-            print("* ", end='')
-        elif(i+j==(n+1)):
-            print("* ", end='')
-        else:
-            print("  ",end='' )
+
+# n=int(input("enter number of rows"))
+# for i in range(1,(n+1)):
+#     for j in range(1,(n+1)):
+#         if(i+j<=2*i):
+#             print("* ", end='')
+#         elif(i+j==(n+1)):
+#             print("* ", end='')
+#         else:
+#             print("  ",end='' )
+#     print()
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end='')
     print()
+for i in range(5,1,-1):
+    for j in range(1,i):
+        print("*",end='')
+    print()
+
 
 
         
